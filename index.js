4711f46c39670475ab10ee73eb53b1e8a3b2af0e
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-08-22 11:25:43 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // console.log("The DOM has loaded")
+    
+    let innerTect = document.getElementById("text");
+    innerTect.textContent = "This is really cool!" // can also use innerHTML
+});
+
+console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
